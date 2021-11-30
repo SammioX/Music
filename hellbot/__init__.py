21f0128a84344038efd1974d1044fd3d88a8eb9e
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2021-11-30 19:16:37 +0530

    Update __init__.py

--- hellbot/__init__.py
+++ hellbot/__init__.py
@@ -1,10 +1,14 @@
+from aiohttp import ClientSession
 from pyrogram import Client
+from Python_ARQ import ARQ
 
-from .config import API_HASH, API_ID, BOT_TOKEN, HELLBOT_SESSION
+from .config import API_HASH, API_ID, ARQ_API_URL, ARQ_API_KEY, BOT_TOKEN, HELLBOT_SESSION
 
 
 hellbot = Client("HellBot_Music", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins"))
 client = Client(HELLBOT_SESSION, API_ID, API_HASH)
+aiosession = ClientSession()
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiosession)
 
 
 run = client.run
